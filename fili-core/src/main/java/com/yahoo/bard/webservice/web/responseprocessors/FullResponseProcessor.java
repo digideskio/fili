f18a92975d51a61724f24f90245158cf166a9adb
Jiaqi Liu <2257440489@qq.com>
2017-05-17 09:33:47 -0700

    add FullResponseProcessor interface that extends ResponseProcessor

---
+++ fili-core/src/main/java/com/yahoo/bard/webservice/web/responseprocessors/FullResponseProcessor.java
@@ -0,0 +1,11 @@
+// Copyright 2017 Yahoo Inc.
+// Licensed under the terms of the Apache license. Please see LICENSE.md file distributed with this work for terms.
+package com.yahoo.bard.webservice.web.responseprocessors;
+
+/**
+ * Response processor for partial data V2 and cache data V3.
+ * <p>
+ * The full response processor also process header information about query response.
+ */
+public interface FullResponseProcessor extends ResponseProcessor {
+}
